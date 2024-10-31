2e73f27513bd7452272056cdf82c000beac382a3
davirodriguesme <160144043+davirodriguesme@users.noreply.github.com>
2024-10-31 09:29:32 -0300

    Add files via upload

---
+++ SQL Editor22.sql
@@ -0,0 +1,21 @@
+﻿create database longa_vida;
+use longa_vida
+
+
+create database plano
+ (
+  numero char(2) primary key not null,
+  descrição char(30),
+  valor decimal(10,2)
+  );
+  
+create database cliente
+ (
+ plano char(2) not null,
+ nome char(40) primary key not null,
+ Endereço char(35),
+ cidade char(20),
+ estado char(2),
+ cep char (9)
+ );
+  
